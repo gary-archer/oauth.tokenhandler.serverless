1d027c2acd7850fbca4d0508b723106077c64915
gary <gary@MACSTATION.local>
2022-03-15 20:52:55 +0000

    Updates

--- test/runHttpWorkflow.sh
+++ test/runHttpWorkflow.sh
@@ -5,7 +5,7 @@
 #####################################################################
 
 WEB_BASE_URL='https://web.authsamples-dev.com'
-OAUTH_AGENT_BASE_URL='https://tokenhandler.authsamples-dev.com/oauth-agent'
+TOKEN_HANDLER_BASE_URL='https://tokenhandler.authsamples-dev.com'
 LOGIN_BASE_URL='https://login.authsamples.com'
 COOKIE_PREFIX=mycompany
 TEST_USERNAME='guestuser@mycompany.com'
@@ -54,24 +54,83 @@ function apiError() {
 }
 
 #
-# Act as the SPA by sending an OPTIONS request, then verifying that we get the expected results
+# Start an authenticated user session
 #
 echo "*** Session ID is $SESSION_ID"
-echo "*** Requesting cross origin access"
-HTTP_STATUS=$(curl -i -s -X OPTIONS "$OAUTH_AGENT_BASE_URL/login/start" \
+
+#
+# First make an OPTIONS request for an invalid route
+#
+echo '1. OPTIONS request for an invalid route ...'
+HTTP_STATUS=$(curl -i -s -X OPTIONS "$TOKEN_HANDLER_BASE_URL/badpath" \
+-o $RESPONSE_FILE -w '%{http_code}')
+if [ "$HTTP_STATUS" != '204' ]; then
+  echo "*** A request with an invalid route returned an unexpected HTTP status: $HTTP_STATUS"
+  apiError "$BODY"
+  exit
+fi
+
+#
+# Next verify that an OPTIONS request for an untrusted origin does not return CORS headers
+#
+echo '2. OPTIONS request for an untrusted origin ...'
+HTTP_STATUS=$(curl -i -s -X OPTIONS "$TOKEN_HANDLER_BASE_URL/api/companies" \
+-o $RESPONSE_FILE -w '%{http_code}')
+if [ "$HTTP_STATUS" != '204' ]; then
+  echo "*** OPTIONS request for an untrusted origin returned an unexpected HTTP status: $HTTP_STATUS"
+  exit
+fi
+ALLOW_ORIGIN=$(getHeaderValue 'access-control-allow-origin')
+if [ "$ALLOW_ORIGIN" != '' ]; then
+  echo '*** OPTIONS request for an untrusted origin returned CORS headers unexpectedly'
+  exit
+fi
+
+#
+# Act as the SPA by sending an OPTIONS request, then verifying that we get the expected results
+#
+echo '3. Requesting cross origin access'
+HTTP_STATUS=$(curl -i -s -X OPTIONS "$TOKEN_HANDLER_BASE_URL/oauth-agent/login/start" \
 -H "origin: $WEB_BASE_URL" \
 -H "access-control-request-headers: x-mycompany-api-client, x-mycompany-session-id" \
 -o $RESPONSE_FILE -w '%{http_code}')
-if [ "$HTTP_STATUS" != '200'  ] && [ "$HTTP_STATUS" != '204' ]; then
+if [ "$HTTP_STATUS" != '204'  ]; then
   echo "*** Problem encountered requesting cross origin access, status: $HTTP_STATUS"
+  exit
+fi
+ALLOW_ORIGIN=$(getHeaderValue 'access-control-allow-origin')
+if [ "$ALLOW_ORIGIN" != "$WEB_BASE_URL" ]; then
+  echo '*** OPTIONS request for an untrusted origin returned an unexpected allow-origin header'
+  exit
+fi
+ALLOW_CREDENTIALS=$(getHeaderValue 'access-control-allow-credentials')
+if [ "$ALLOW_CREDENTIALS" != 'true' ]; then
+  echo '*** OPTIONS request for an untrusted origin returned an unexpected allow-credentials header'
+  exit
+fi
+ALLOWED_HEADERS=$(getHeaderValue 'access-control-allow-headers')
+if [ "$ALLOWED_HEADERS" != 'x-mycompany-api-client, x-mycompany-session-id' ]; then
+  echo '*** OPTIONS request for an untrusted origin returned an unexpected allow-headers header'
+  exit
+fi
+
+#
+# Verify that a GET request for an invalid route returns a 404 error
+#
+echo '4. GET request with an invalid route ...'
+HTTP_STATUS=$(curl -i -s -X GET "$TOKEN_HANDLER_BASE_URL/badpath" \
+-o $RESPONSE_FILE -w '%{http_code}')
+if [ $HTTP_STATUS -ne '404' ]; then
+  echo "*** GET request with an invalid route returned an unexpected HTTP status: $HTTP_STATUS"
+  apiError "$BODY"
   exit
 fi
 
 #
 # Act as the SPA by calling the OAuth Agent to start a login and get the request URI
 #
-echo "*** Creating login URL ..."
-HTTP_STATUS=$(curl -i -s -X POST "$OAUTH_AGENT_BASE_URL/login/start" \
+echo '5. Creating login URL ...'
+HTTP_STATUS=$(curl -i -s -X POST "$TOKEN_HANDLER_BASE_URL/oauth-agent/login/start" \
 -H "origin: $WEB_BASE_URL" \
 -H 'accept: application/json' \
 -H 'x-mycompany-api-client: httpTest' \
@@ -92,7 +151,7 @@ STATE_COOKIE=$(getCookieValue "$COOKIE_PREFIX-state")
 #
 # Next invoke the redirect URI to start a login
 #
-echo "*** Following login redirect ..."
+echo '6. Following login redirect ...'
 HTTP_STATUS=$(curl -i -L -s "$AUTHORIZATION_REQUEST_URL" -o $RESPONSE_FILE -w '%{http_code}')
 if [ "$HTTP_STATUS" != '200' ]; then
   echo "*** Problem encountered using the OpenID Connect authorization URL, status: $HTTP_STATUS"
@@ -109,7 +168,7 @@ COGNITO_XSRF_TOKEN=$(getCookieValue 'XSRF-TOKEN' | cut -d ' ' -f 2)
 #
 # We can now post a password credential, and the form fields used are Cognito specific
 #
-echo "*** Posting credentials to sign in the test user ..."
+echo '7. Posting credentials to sign in the test user ...'
 HTTP_STATUS=$(curl -i -s -X POST "$LOGIN_POST_LOCATION" \
 -H "origin: $LOGIN_BASE_URL" \
 --cookie "XSRF-TOKEN=$COGNITO_XSRF_TOKEN" \
@@ -130,8 +189,8 @@ AUTHORIZATION_RESPONSE_URL=$(getHeaderValue 'location')
 #
 # Next we end the login by asking the server to do an authorization code grant
 #
-echo "*** Finishing the login by processing the authorization code ..."
-HTTP_STATUS=$(curl -i -s -X POST "$OAUTH_AGENT_BASE_URL/login/end" \
+echo '8. Finishing the login by processing the authorization code ...'
+HTTP_STATUS=$(curl -i -s -X POST "$TOKEN_HANDLER_BASE_URL/oauth-agent/login/end" \
 -H "origin: $WEB_BASE_URL" \
 -H 'content-type: application/json' \
 -H 'accept: application/json' \
@@ -157,10 +216,24 @@ ID_COOKIE=$(getCookieValue "$COOKIE_PREFIX-id")
 CSRF_COOKIE=$(getCookieValue "$COOKIE_PREFIX-csrf")
 
 #
+# Verify that a GET request with an error returns readable error responses
+#
+echo '9. GET request with a valid access cookie returns JSON data ...'
+HTTP_STATUS=$(curl -i -s -X GET "$TOKEN_HANDLER_BASE_URL/api/companies" \
+-H "origin: $WEB_BASE_URL" \
+-H "cookie: $COOKIE_PREFIX-at=$ACCESS_COOKIE" \
+-o $RESPONSE_FILE -w '%{http_code}')
+if [ "$HTTP_STATUS" != '200' ]; then
+  echo "*** GET request with a valid access cookie returned an unexpected HTTP status: $HTTP_STATUS"
+  apiError "$BODY"
+  exit
+fi
+
+#
 # Next expire the access token in the secure cookie, for test purposes
 #
-echo "*** Expiring the access token ..."
-HTTP_STATUS=$(curl -i -s -X POST "$OAUTH_AGENT_BASE_URL/expire" \
+echo '10. Expiring the access token ...'
+HTTP_STATUS=$(curl -i -s -X POST "$TOKEN_HANDLER_BASE_URL/oauth-agent/expire" \
 -H "origin: $WEB_BASE_URL" \
 -H 'content-type: application/json' \
 -H 'accept: application/json' \
@@ -180,8 +253,8 @@ ACCESS_COOKIE=$(getCookieValue "$COOKIE_PREFIX-at")
 #
 # Next try to refresh the access token
 #
-echo "*** Calling refresh to get a new access token ..."
-HTTP_STATUS=$(curl -i -s -X POST "$OAUTH_AGENT_BASE_URL/refresh" \
+echo '11. Calling refresh to get a new access token ...'
+HTTP_STATUS=$(curl -i -s -X POST "$TOKEN_HANDLER_BASE_URL/oauth-agent/refresh" \
 -H "origin: $WEB_BASE_URL" \
 -H 'accept: application/json' \
 -H 'x-mycompany-api-client: httpTest' \
@@ -201,8 +274,8 @@ ID_COOKIE=$(getCookieValue "$COOKIE_PREFIX-id")
 #
 # Next expire both the access token and refresh token in the secure cookies, for test purposes
 #
-echo "*** Expiring the refresh token ..."
-HTTP_STATUS=$(curl -i -s -X POST "$OAUTH_AGENT_BASE_URL/expire" \
+echo '12. Expiring the refresh token ...'
+HTTP_STATUS=$(curl -i -s -X POST "$TOKEN_HANDLER_BASE_URL/oauth-agent/expire" \
 -H "origin: $WEB_BASE_URL" \
 -H 'content-type: application/json' \
 -H 'accept: application/json' \
@@ -223,8 +296,8 @@ REFRESH_COOKIE=$(getCookieValue "$COOKIE_PREFIX-rt")
 #
 # Next try to refresh the token and we should get an invalid_grant error
 #
-echo "*** Trying to refresh the access token when the session is expired ..."
-HTTP_STATUS=$(curl -i -s -X POST "$OAUTH_AGENT_BASE_URL/refresh" \
+echo '13. Trying to refresh the access token when the session is expired ...'
+HTTP_STATUS=$(curl -i -s -X POST "$TOKEN_HANDLER_BASE_URL/oauth-agent/refresh" \
 -H "origin: $WEB_BASE_URL" \
 -H 'accept: application/json' \
 -H 'x-mycompany-api-client: httpTest' \
@@ -241,8 +314,8 @@ fi
 #
 # Next make a logout request
 #
-echo "*** Calling logout to clear cookies and get the end session request URL ..."
-HTTP_STATUS=$(curl -i -s -X POST "$OAUTH_AGENT_BASE_URL/logout" \
+echo '14. Calling logout to clear cookies and get the end session request URL ...'
+HTTP_STATUS=$(curl -i -s -X POST "$TOKEN_HANDLER_BASE_URL/oauth-agent/logout" \
 -H "origin: $WEB_BASE_URL" \
 -H 'accept: application/json' \
 -H 'x-mycompany-api-client: httpTest' \
